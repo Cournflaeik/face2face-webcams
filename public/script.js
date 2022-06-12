612dfb04fb1c31e909c78de31bb7c26d68486d4b
Cournflaeik <elien.degeetere@gmail.com>
2022-06-12 23:34:28 +0200

    fix:test10

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
+const myPeer = new Peer({
     secure: true,
     host: 'face2face-webcams.herokuapp.com/',
     port: 9000,
